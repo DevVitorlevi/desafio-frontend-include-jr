30c84f38c2e4fdfcc791e3702d0f18ea4ca2a23a
Vitor Levi <levivitor8@gmail.com>
2026-08-29 11:23:26 -0300

    feat(about): add About

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { About } from "./components/sections/About";
 import { Skills } from "./components/sections/Skills";
 import { Experience } from "./components/sections/Experience";
 import { Projects } from "./components/sections/Projects";
-import { Contact } from "./components/sections/Contact";
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
         <Skills />
         <Experience />
         <Projects />
-        <Contact />
       </main>
     </ThemeProvider>
   );
