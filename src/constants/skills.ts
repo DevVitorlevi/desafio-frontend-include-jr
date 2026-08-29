14de8831e7156e3e4a6ddb533187b18abbb2185a
Vitor Levi <levivitor8@gmail.com>
2026-08-29 16:15:45 -0300

    feat(constants): add skills

---
+++ src/constants/skills.ts
@@ -0,0 +1,116 @@
+import { SkillCategory } from "@/types";
+
+export const skills: SkillCategory[] = [
+  {
+    category: "frontend",
+    label: "Frontend",
+    techs: [
+      { name: "HTML5", icon: "https://skills.syvixor.com/api/icons?i=html" },
+      { name: "CSS3", icon: "https://skills.syvixor.com/api/icons?i=css3" },
+      { name: "JavaScript", icon: "https://skills.syvixor.com/api/icons?i=js" },
+      { name: "TypeScript", icon: "https://skills.syvixor.com/api/icons?i=ts" },
+      {
+        name: "React.js",
+        icon: "https://skills.syvixor.com/api/icons?i=reactjs",
+      },
+      {
+        name: "Next.js",
+        icon: "https://skills.syvixor.com/api/icons?i=nextjs",
+      },
+      {
+        name: "Tailwind",
+        icon: "https://skills.syvixor.com/api/icons?i=tailwind",
+      },
+      {
+        name: "Styled",
+        icon: "https://skills.syvixor.com/api/icons?i=styledcomponents",
+      },
+      {
+        name: "ShadCN/UI",
+        icon: "https://skills.syvixor.com/api/icons?i=shadcnui",
+      },
+      {
+        name: "Cypress.io",
+        icon: "https://skills.syvixor.com/api/icons?i=cypress",
+      },
+    ],
+  },
+  {
+    category: "backend",
+    label: "Backend",
+    techs: [
+      {
+        name: "Node.js",
+        icon: "https://skills.syvixor.com/api/icons?i=nodejs",
+      },
+      {
+        name: "Fastify",
+        icon: "https://skills.syvixor.com/api/icons?i=fastify",
+      },
+      {
+        name: "Express.js",
+        icon: "https://skills.syvixor.com/api/icons?i=express",
+      },
+      { name: "MySQL", icon: "https://skills.syvixor.com/api/icons?i=mysql" },
+      {
+        name: "PostgreSQL",
+        icon: "https://skills.syvixor.com/api/icons?i=postgresql",
+      },
+      {
+        name: "MongoDB",
+        icon: "https://skills.syvixor.com/api/icons?i=mongodb",
+      },
+      { name: "Prisma", icon: "https://skills.syvixor.com/api/icons?i=prisma" },
+      { name: "JWT", icon: "https://skills.syvixor.com/api/icons?i=jwt" },
+      { name: "Vitest", icon: "https://skills.syvixor.com/api/icons?i=vitest" },
+      { name: "Docker", icon: "https://skills.syvixor.com/api/icons?i=docker" },
+      {
+        name: "Swagger",
+        icon: "https://skills.syvixor.com/api/icons?i=swagger",
+      },
+    ],
+  },
+  {
+    category: "tools",
+    label: "Ferramentas",
+    techs: [
+      {
+        name: "VS Code",
+        icon: "https://skills.syvixor.com/api/icons?i=vscode",
+      },
+      {
+        name: "Github",
+        icon: "https://skills.syvixor.com/api/icons?i=github",
+      },
+      {
+        name: "Git",
+        icon: "https://skills.syvixor.com/api/icons?i=git",
+      },
+      {
+        name: "Postman",
+        icon: "https://skills.syvixor.com/api/icons?i=postman",
+      },
+      {
+        name: "DBeaver",
+        icon: "https://skills.syvixor.com/api/icons?i=dbeaver",
+      },
+      {
+        name: "Trello",
+        icon: "https://skills.syvixor.com/api/icons?i=trello",
+      },
+    ],
+  },
+  {
+    category: "methodologies",
+    label: "Metodologias",
+    techs: [],
+    methodologies: [
+      "Clean Code",
+      "Clean Architecture",
+      "DDD",
+      "SOLID",
+      "TDD",
+      "MVC",
+    ],
+  },
+];
